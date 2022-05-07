075277f0eca9b682dffcfde5757ebeaedbcbccca
Dorjoo <ddorjoo268@gmail.com>
2022-05-07 15:17:16 +0800

    asdf

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -46,13 +46,12 @@ export default async function handler(req, res) {
       data: response.data,
     });
   } catch (e) {
-    return res
-      .status(500)
-      .send({
-        message: e,
-        sheet_id: process.env.GOOGLE_SHEET_ID,
-        sheet_key: process.env.GOOGLE_PRIVATE_KEY,
-        email: process.env.GOOGLE_CLIENT_EMAIL,
-      });
+    return res.status(500).send({
+      message: e,
+      sheet_id: process.env.GOOGLE_SHEET_ID,
+      sheet_key: process.env.GOOGLE_PRIVATE_KEY,
+      email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    });
   }
 }
