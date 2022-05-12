647e6c1c891db09fcf0c09bebaeaf085f9994d9a
fl1pHover <bishu.hover@gmail.com>
2022-05-12 21:00:18 +0800

    asdf

--- src/components/Loan.js
+++ src/components/Loan.js
@@ -1,5 +1,6 @@
 import Title from './Title';
 import Image from 'next/image';
+import Link from 'next/link';
 const Loans = () => {
 	return (
 		<div className="loans relative">
@@ -13,46 +14,50 @@ const Loans = () => {
 			</div>
 			<span className="absolute inset-0 bg_color"></span>
 			<div className="flex items-center justify-center relative z-30 mt-12">
-				<div className="mr-24 loan_grid">
-					<div className=" relative">
-						<Image
-							src={'/img/smile01.jpg'}
-							alt="loan"
-							layout="responsive"
-							width={500}
-							height={320}
-						/>
+				<Link href={'/loans/internal'}>
+					<div className="mr-24 loan_grid cursor-pointer">
+						<div className=" relative">
+							<Image
+								src={'/img/smile01.jpg'}
+								alt="loan"
+								layout="responsive"
+								width={500}
+								height={320}
+							/>
+						</div>
+						<div className="px-6 py-8 bg-white">
+							<h5 className="text_primary_color uppercase mb-2">
+								<b> зээл</b>
+							</h5>
+							<h2 className="capitalize font-bold text-3xl tracking-wider mb-6 font-medium">
+								гадаад харилцагч
+							</h2>
+							<p>6-р сар 1, 2022</p>
+						</div>
 					</div>
-					<div className="px-6 py-8 bg-white">
-						<h5 className="text_primary_color uppercase mb-2">
-							<b> зээл</b>
-						</h5>
-						<h2 className="capitalize font-bold text-3xl tracking-wider mb-6 font-medium">
-							гадаад харилцагч
-						</h2>
-						<p>6-р сар 1, 2022</p>
+				</Link>
+				<Link href="/loans/external">
+					<div className="loan_grid cursor-pointer">
+						<div className=" relative">
+							<Image
+								src={'/img/smile01.jpg'}
+								alt="loan"
+								layout="responsive"
+								width={500}
+								height={320}
+							/>
+						</div>
+						<div className="px-6 py-8 bg-white">
+							<h5 className="text_primary_color uppercase mb-2">
+								<b> зээл</b>
+							</h5>
+							<h2 className="capitalize font-bold text-3xl tracking-wider mb-6 font-medium">
+								дотоод харилцагч
+							</h2>
+							<p>6-р сар 1, 2022</p>
+						</div>
 					</div>
-				</div>
-				<div className="loan_grid">
-					<div className=" relative">
-						<Image
-							src={'/img/smile01.jpg'}
-							alt="loan"
-							layout="responsive"
-							width={500}
-							height={320}
-						/>
-					</div>
-					<div className="px-6 py-8 bg-white">
-						<h5 className="text_primary_color uppercase mb-2">
-							<b> зээл</b>
-						</h5>
-						<h2 className="capitalize font-bold text-3xl tracking-wider mb-6 font-medium">
-							дотоод харилцагч
-						</h2>
-						<p>6-р сар 1, 2022</p>
-					</div>
-				</div>
+				</Link>
 			</div>
 		</div>
 	);
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,43 +1,56 @@
-import Title from "./Title";
-import Image from "next/image";
+import Title from './Title';
+import Image from 'next/image';
+import Link from 'next/link';
 const Projects = () => {
-  return (
-    <div className="mx-auto  projects mb-32">
-      <Title title={"Хотхоны сонголт"} subtitle={"ТӨСӨЛ"} items={"center"} />
-      <div className="grid grid-cols-2 mt-8 gap-20 p-3">
-        <div className="project_item_1">
-          <div className=" mb-6 relative ">
-            <Image
-              src={"/img/kh4.jpg"}
-              alt="project"
-              layout="responsive"
-              width={384}
-              height={256}
-            />
-          </div>
-          <h2 className="text-2xl font-semibold mb-4">Хадат Вилла 4</h2>
-          <button className="uppercase text-sm tracking-widest font-medium">
-            цааш
-          </button>
-        </div>
-        <div className="project_item_2">
-          <div className=" mb-6 relative">
-            <Image
-              src={"/img/project/kh2.jpg"}
-              alt="project"
-              layout="responsive"
-              width={384}
-              height={256}
-            />
-          </div>
-          <h2 className="text-2xl font-semibold mb-4">Хадат Вилла 2</h2>
-          <button className="uppercase text-sm tracking-widest font-medium">
-            цааш
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="mx-auto  projects mb-32">
+			<Title
+				title={'Хотхоны сонголт'}
+				subtitle={'ТӨСӨЛ'}
+				items={'center'}
+			/>
+			<div className="grid grid-cols-2 mt-8 gap-20 p-3">
+				<div className="project_item_1">
+					<div className=" mb-6 relative ">
+						<Image
+							src={'/img/kh4.jpg'}
+							alt="project"
+							layout="responsive"
+							width={384}
+							height={256}
+						/>
+					</div>
+					<h2 className="text-2xl font-semibold mb-4">
+						Хадат Вилла 4
+					</h2>
+					<Link href={'/project'}>
+						<button className="uppercase text-sm tracking-widest font-medium">
+							цааш
+						</button>
+					</Link>
+				</div>
+				<div className="project_item_2">
+					<div className=" mb-6 relative">
+						<Image
+							src={'/img/project/kh2.jpg'}
+							alt="project"
+							layout="responsive"
+							width={384}
+							height={256}
+						/>
+					</div>
+					<h2 className="text-2xl font-semibold mb-4">
+						Хадат Вилла 2
+					</h2>
+					<Link href={'/project/khadat-villa-2'}>
+						<button className="uppercase text-sm tracking-widest font-medium">
+							цааш
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
