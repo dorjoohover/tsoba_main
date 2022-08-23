ff1d3200fd0a72e211f01b1446c07458f0a6586d
dorjoo <dorjoohover@gmail.com>
2022-08-23 20:29:16 +0800

    added clg

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -99,7 +99,7 @@ export default async function handler(req, res) {
           ],
         ],
       },
-    })
+    }).catch((err) => console.log(err))
 
     return res.status(200).json({
       data: response.data,
